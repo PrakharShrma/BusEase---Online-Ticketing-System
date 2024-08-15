cb5201223a7e39c27931738262264db6a43188b2
Prakhar Sharma <161190891+PrakharShrma@users.noreply.github.com>
2024-08-15 12:00:00 +0530

    Update nbstc.sql

--- BusEase---Online-Ticketing-System/nbstc.sql
+++ BusEase---Online-Ticketing-System/nbstc.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `nbstc` 
-USE `nbstc`;
+CREATE DATABASE  IF NOT EXISTS `isbt` 
+USE `isbt`;
 
 
 DROP TABLE IF EXISTS `bookings`;
@@ -23,25 +23,11 @@ CREATE TABLE `bookings` (
   CONSTRAINT `rid` FOREIGN KEY (`route_id`) REFERENCES `routes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `uid` FOREIGN KEY (`user_id`) REFERENCES `user_dtls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `bookings`
---
 
-LOCK TABLES `bookings` WRITE;
-/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
-INSERT INTO `bookings` VALUES (1,1,'15980','SLG-MLD','Siliguri','Malda','8:30 pm','750','Rohit Singh','1',2,'Pending'),(2,1,'15406','SLG-KOL','Siliguri','Kolkata','6:30 Pm','800','Priti Saha','2',1,'Approved');
-/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `routes`
---
 
 DROP TABLE IF EXISTS `routes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `routes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `busno` varchar(45) NOT NULL,
@@ -54,23 +40,9 @@ CREATE TABLE `routes` (
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `routes`
---
-
-LOCK TABLES `routes` WRITE;
-/*!40000 ALTER TABLE `routes` DISABLE KEYS */;
-INSERT INTO `routes` VALUES (1,'15406','SLG-KOL','Siliguri','Kolkata','6:30 Pm','800'),(2,'15980','SLG-MLD','Siliguri','Malda','8:30 pm','750'),(4,'14502','APD-SLG','Alipurduar','Siliguri','11:30 am','140'),(5,'15302','DHH-RGJ','Dinhata','Raiganj','7:30 am','160'),(6,'15304','SLG-DJ','Siliguri','Darjeeling','8:30 am','200'),(7,'15305','DJ-SLG','Darjeeling','Siliguri','10:30 am','200'),(8,'15306','SLG-KGN','Siliguri','Kurseong','9:30 am','150'),(9,'15308','KPG-SLG','Kalimpong','Siliguri','1:00 pm','170'),(10,'15309','SLG-JPG','Siliguri','Jalpaiguri','10:00 am','80'),(11,'15311','JPG-HDB','Jalpaiguri','Haldibari','8:30 am','150'),(12,'15312','MLBZ-SLG','Malbazar','Siliguri','7:30 am','200'),(13,'15314','SLG-MHBA','Siliguri','Mathabhanga','8:00 am','300');
-/*!40000 ALTER TABLE `routes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_dtls`
---
 
 DROP TABLE IF EXISTS `user_dtls`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `user_dtls` (
   `id` int NOT NULL AUTO_INCREMENT,
   `full_name` varchar(45) NOT NULL,
@@ -78,25 +50,4 @@ CREATE TABLE `user_dtls` (
   `password` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `user_dtls`
---
-
-LOCK TABLES `user_dtls` WRITE;
-/*!40000 ALTER TABLE `user_dtls` DISABLE KEYS */;
-INSERT INTO `user_dtls` VALUES (1,'Sonu Sharma','sonu@gmail.com','1234'),(2,'Aditya Das','aditya@gmail.com','1234'),(3,'Harish Sharma','harish@gmail.com','1234'),(4,'Payal Ray','payal@gmail.com','1234');
-/*!40000 ALTER TABLE `user_dtls` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-10-25 16:11:51
